9c0d708d288948fce6e5f9dac38778ae9d04b049
Giuseppe <40606360+giuseppebalbi@users.noreply.github.com>
2019-10-11 13:26:52 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,4 +1,65 @@
 <?php
+
+// Load composer
+require_once __DIR__ . '/vendor/autoload.php';
+// Add you bot's API key and name
+$bot_api_key  = '921851310:AAHGBNHdtGve5JlLWhkA1Wo2qTXHU6BcTKI';
+$bot_username = 'AdventureHades_bot';
+// Define all IDs of admin users in this array (leave as empty array if not used)
+$admin_users = [
+//    123,
+];
+// Define all paths for your custom commands in this array (leave as empty array if not used)
+$commands_paths = [
+    __DIR__ . '/Commands/',
+];
+// Enter your MySQL database credentials
+$mysql_credentials = [
+    'host'     => '89.46.111.68',
+    'user'     => 'Sql1228856',
+    'password' => '0218k30204',
+    'database' => 'Sql1228856_5',
+];
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+    // Add commands paths containing your custom commands
+    $telegram->addCommandsPaths($commands_paths);
+    // Enable admin users
+    $telegram->enableAdmins($admin_users);
+    // Enable MySQL
+    $telegram->enableMySql($mysql_credentials);
+    // Logging (Error, Debug and Raw Updates)
+    //Longman\TelegramBot\TelegramLog::initErrorLog(__DIR__ . "/{$bot_username}_error.log");
+    //Longman\TelegramBot\TelegramLog::initDebugLog(__DIR__ . "/{$bot_username}_debug.log");
+    //Longman\TelegramBot\TelegramLog::initUpdateLog(__DIR__ . "/{$bot_username}_update.log");
+    // If you are using a custom Monolog instance for logging, use this instead of the above
+    //Longman\TelegramBot\TelegramLog::initialize($your_external_monolog_instance);
+    // Set custom Upload and Download paths
+    //$telegram->setDownloadPath(__DIR__ . '/Download');
+    //$telegram->setUploadPath(__DIR__ . '/Upload');
+    // Here you can set some command specific parameters
+    // e.g. Google geocode/timezone api key for /date command
+    //$telegram->setCommandConfig('date', ['google_api_key' => 'your_google_api_key_here']);
+    // Botan.io integration
+    //$telegram->enableBotan('your_botan_token');
+    // Requests Limiter (tries to prevent reaching Telegram API limits)
+    $telegram->enableLimiter();
+    // Handle telegram webhook request
+    $telegram->handle();
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Silence is golden!
+    //echo $e;
+    // Log telegram errors
+    Longman\TelegramBot\TelegramLog::error($e);
+}
+
+
+
+
+
+
+
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
