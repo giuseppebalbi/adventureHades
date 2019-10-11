a7dfa0e5153b8dbc8f3b906a056f2ee1b9f51e9f
Giuseppe <40606360+giuseppebalbi@users.noreply.github.com>
2019-10-11 12:16:24 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -43,6 +43,6 @@ else
 
 
 
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
